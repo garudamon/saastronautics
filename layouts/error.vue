1af316d8cf3733c99c926166d6e9b7538baf088e
Ali Imron <ali@kanosolution.com>
2020-07-31 21:11:41 +0700

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="error d-flex justify-content-center align-items-center">
+    <h3 v-if="error.statusCode === 404">Page not found</h3>
+    <h3 v-else>An error occurred</h3>
+    <nuxt-link :to="'/'" class="btn btn-primary btn-lg">Back to Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'nostyle'
+}
+</script>
+
+<style lang="scss" scoped>
+.error {
+  height: 100vh;
+  position: relative;
+  h3 {
+    color: tomato;
+    font-family: 'Courier New', Courier, monospace;
+  }
+  .btn {
+    position: absolute;
+    bottom: 2.5rem;
+    left: 5rem;
+    right: 5rem;
+  }
+}
+</style>
---
+++ layouts/nostyle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fullheight">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.fullheight {
+  height: 100vh;
+}
+</style>
