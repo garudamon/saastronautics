ea021a079c9f175fa5460a7241c164d6ac5d5e85
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-08-27 10:48:43 +0700

    fixing title Frank Valensco and fixing height div

--- components/TeamMember.vue
+++ components/TeamMember.vue
@@ -5,18 +5,26 @@
       v-for="item in member"
       :key="`${item.name}-${item.position}`"
     >
-      <div class="team-member py-3 py-md-4 mx-md-2 px-3 px-md-4">
+      <div class="h-100 team-member py-3 py-md-4 mx-md-2 px-3 px-md-4">
         <div class="p-3">
           <img :src="item.imageUrl" :alt="`${item.name}'s photo`" />
         </div>
         <p class="font-weight-bold mt-3 mb-0">{{ item.name }}</p>
         <p class="text-uppercase text-red m-0">{{ item.position }}</p>
         <p class="py-4">{{ item.description }}</p>
-        <p>
-          <a href="#"><i class="fa px-3 fa-facebook"></i></a>
-          <a href="#"><i class="fa px-3 fa-instagram"></i></a>
-          <a href="#"><i class="fa px-3 fa-linkedin"></i></a>
-        </p>
+        <div class="bottom-div pb-3">
+          <p>
+            <a href="#">
+              <i class="fa px-3 fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i class="fa px-3 fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="fa px-3 fa-linkedin"></i>
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -39,6 +47,14 @@ export default {
   }
   a {
     color: #ff4370;
+  }
+  .bottom-div {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
   }
 }
 </style>
