881401faa4dc6abcb1decb47fa391d5c3980d8a9
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-09-01 10:14:16 +0700

    put api on signup page

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -3,21 +3,39 @@
     <div class="px-5 py-3">
       <img src="~/assets/images/logo.png" alt />
     </div>
-    <form class="p-3">
+    <form class="p-3" @submit.prevent="signUp">
       <input
         type="text"
         class="form-control form-round form-gray border-0 mb-4"
+        placeholder="enter your first name"
+        v-model="form.firstName"
+        required
+      />
+      <input
+        type="text"
+        class="form-control form-round form-gray border-0 mb-4"
+        placeholder="enter your last name"
+        v-model="form.lastName"
+      />
+      <input
+        type="email"
+        class="form-control form-round form-gray border-0 mb-4"
         placeholder="enter your email address"
+        v-model="form.email"
+        required
       />
       <input
         type="password"
         class="form-control form-round form-gray border-0 mb-4"
         placeholder="enter your password"
+        required
       />
       <input
         type="password"
         class="form-control form-round form-gray border-0 mb-4"
         placeholder="confirm your password"
+        v-model="form.newPassword"
+        required
       />
       <div class="d-flex align-items-center">
         <p class="flex-grow-1 text-left m-0">
@@ -32,7 +50,37 @@
 
 <script>
 export default {
-  layout: 'auth'
+  layout: 'auth',
+  data: () => ({
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      newPassword: ''
+    }
+  }),
+  methods: {
+    callError(error = '') {
+      this.$swal('Failed', error, 'error')
+    },
+    signUp() {
+      console.log(this.form)
+      this.$axios
+        .post('/customer/register', this.form)
+        .then(res => {
+          //Perform Success Action
+          this.$router.push('/auth/signin')
+          console.log(res.data)
+        })
+        .catch(error => {
+          // error.response.status Check status code
+          console.log(error)
+        })
+        // .finally(() => {
+          //Perform action in always
+        // })
+    }
+  }
 }
 </script>
 
