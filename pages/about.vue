806dae74ec3550e8a8b72c4b523ca792b38f81cf
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-09-03 07:15:11 +0700

    change content about Frank Valensco

--- pages/about.vue
+++ pages/about.vue
@@ -114,7 +114,7 @@ export default {
           name: 'Frank Valensco',
           position: 'Co-Founder & Head of Product',
           description:
-            'Head of Development at Saastronautics. CEO and Co- founder of Sekawan Solution, an IT consultant company based in Jakarta, Indonesia. Have been working on developing solutions and products for the last 7 years to help clients grow their business.'
+            'Head of Product at Saastronautics. CEO and Co-founder of Sekawan Solution, a Creative IT Company based in Jakarta, Indonesia. Have been working on developing solutions and products for the last 7 years to help clients grow their business.'
         },
         {
           imageUrl: require('~/assets/images/about/gerard-brand.jpeg'),
