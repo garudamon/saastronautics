bb99c5b2a5b900ba164da1d4750dee16d5dcb3aa
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-09-09 21:13:21 +0700

    add validation form on sign up page

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -3,53 +3,89 @@
     <div class="px-5 py-3">
       <img src="~/assets/images/logo.png" alt />
     </div>
-    <form class="p-3" @submit.prevent="signUp">
-      <input
-        type="text"
-        class="form-control form-round form-gray border-0 mb-4"
-        placeholder="enter your first name"
-        v-model="form.firstName"
-        required
-      />
-      <input
-        type="email"
-        class="form-control form-round form-gray border-0 mb-4"
-        placeholder="enter your email address"
-        v-model="form.email"
-        required
-      />
-      <input
-        type="password"
-        class="form-control form-round form-gray border-0 mb-4"
-        placeholder="enter your password"
-        required
-      />
-      <input
-        type="password"
-        class="form-control form-round form-gray border-0 mb-4"
-        placeholder="confirm your password"
-        v-model="form.newPassword"
-        required
-      />
-      <div class="d-flex align-items-center">
-        <p class="flex-grow-1 text-left m-0">
-          <nuxt-link to="/auth/signin">Already have an account?</nuxt-link>
-        </p>
+    <ValidationObserver v-slot="{ handleSubmit }">
+      <form class="p-3" @submit.prevent="handleSubmit(signUp)">
+        <ValidationProvider name="Name" rules="required|alpha" v-slot="{ errors }">
+          <div class="form-group mb-4">
+            <input
+              type="text"
+              class="form-control form-round form-gray border-0"
+              placeholder="enter your first name"
+              v-model="form.firstName"
+            />
+            <small class="form-text text-left">{{ errors[0] }}</small>
+          </div>
+        </ValidationProvider>
+        <ValidationProvider name="E-mail" rules="required|email" v-slot="{ errors }">
+          <div class="form-group mb-4">
+            <input
+              type="email"
+              class="form-control form-round form-gray border-0"
+              placeholder="enter your email address"
+              v-model="form.email"
+            />
+            <small class="form-text text-left">{{ errors[0] }}</small>
+          </div>
+        </ValidationProvider>
+        <ValidationProvider name="Password" rules="required" v-slot="{ errors }">
+          <div class="form-group mb-4">
+            <input
+              type="password"
+              class="form-control form-round form-gray border-0"
+              placeholder="enter your password"
+              v-model="form.Password"
+              ref="password"
+            />
+            <small class="form-text text-left">{{ errors[0] }}</small>
+          </div>
+        </ValidationProvider>
+        <ValidationProvider name="Password Confirmation" rules="required|password:@Password" v-slot="{ errors }">
+          <div class="form-group mb-4">
+            <input
+              type="password"
+              class="form-control form-round form-gray border-0"
+              placeholder="confirm your password"
+              v-model="form.newPassword"
+              data-vv-as="password"
+            />
+            <small class="form-text text-left">{{ errors[0] }}</small>
+          </div>
+        </ValidationProvider>
+        <div class="d-flex align-items-center">
+          <p class="flex-grow-1 text-left m-0">
+            <nuxt-link to="/auth/signin">Already have an account?</nuxt-link>
+          </p>
 
-        <button type="submit" class="btn btn-primary btn-md px-4">Sign Up</button>
-      </div>
-    </form>
+          <button type="submit" class="btn btn-primary btn-md px-4">Sign Up</button>
+        </div>
+      </form>
+    </ValidationObserver>
   </div>
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate/dist/vee-validate.full.esm'
+
+extend('password', {
+  params: ['target'],
+  validate(value, { target }) {
+    return value === target;
+  },
+  message: 'Password confirmation does not match'
+});
+
 export default {
   layout: 'auth',
   loading: false,
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   data: () => ({
     form: {
       firstName: '',
       email: '',
+      password: '',
       newPassword: ''
     }
   }),
@@ -100,6 +136,9 @@ p {
   font-size: 0.8rem;
 }
 a {
+  color: var(--primary-color-red);
+}
+small {
   color: var(--primary-color-red);
 }
 .swal-button--cancel {
