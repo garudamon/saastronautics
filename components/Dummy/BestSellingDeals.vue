a2601d53ac4477f1b781fe29b7076f095ff9c5cc
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-10-16 16:07:41 +0700

    fix featured deals

--- components/Dummy/BestSellingDeals.vue
+++ components/Dummy/BestSellingDeals.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="title-2 font-weight-bold mb-4">Featured Deals</h1>
+      </div>
+      <div class="col-12 col-md-10 mx-auto text-center graphik-reg">
+        <p>
+          Don’t miss out on these exclusive deals at an extraordinary low price. 
+        </p>
+      </div>
+    </div>
+    <div class="row my-5">
       <div v-for="n in 3" :key="n" class="col-12 col-md-4 product mb-5 px-4 pb-3">
         <LazyComingSoon />
       </div>
