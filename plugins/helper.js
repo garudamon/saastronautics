14dc693e7e205efc58b72019daa4d0918e9e83f3
Ali Imron <ali@kanosolution.com>
2020-10-18 20:58:38 +0700

    write a better helper

--- plugins/helper.js
+++ plugins/helper.js
@@ -3,46 +3,45 @@ import moment from 'moment'
 const Cookie = require('js-cookie')
 const Helper = {
   install(Vue) {
-    ; (Vue.prototype.$getImage = id => {
+    Vue.prototype.$getImage = id => {
       return `${process.env.API_URL}/product/image/banner/${id}`
-    })(Vue.prototype.$getStaticImage = imageName => {
+    }
+    Vue.prototype.$getStaticImage = imageName => {
       if (process.env.IS_CLOUDFRONT == true) {
         return `${process.env.CLOUD_FRONT_URL}/${imageName}`
       } else {
         return `/images/${imageName}`
       }
-
-    })
-      ,
-      (Vue.prototype.$getProfile = id => {
-        return `${process.env.API_URL}/customer/image/profile/${id}`
-      }),
-      (Vue.prototype.$getImageOther = (id, idx) => {
-        return `${process.env.API_URL}/product/image/other/${id}/${idx}`
-      }),
-      (Vue.prototype.$formattedDate = string => {
-        return moment(string).format('MMM DD YYYY')
-      }),
-      (Vue.prototype.$formattedMoney = number => {
-        if (number == null) number = 0
-        return '$' + number.toLocaleString()
-      }),
-      (Vue.prototype.$affiliateSet = referral => {
-        let existing = Cookie.get('_ref')
-        if (existing != 'undefined') {
-          Cookie.set('_ref', referral, {
-            expires: 30
-          })
-        }
-      }),
-      (Vue.prototype.$affiliateGet = () => {
-        let existing = Cookie.get('_ref')
-        if (existing != 'undefined') {
-          return existing
-        } else {
-          return ''
-        }
-      })
+    }
+    Vue.prototype.$getProfile = id => {
+      return `${process.env.API_URL}/customer/image/profile/${id}`
+    }
+    Vue.prototype.$getImageOther = (id, idx) => {
+      return `${process.env.API_URL}/product/image/other/${id}/${idx}`
+    }
+    Vue.prototype.$formattedDate = string => {
+      return moment(string).format('MMM DD YYYY')
+    }
+    Vue.prototype.$formattedMoney = number => {
+      if (number == null) number = 0
+      return '$' + number.toLocaleString()
+    }
+    Vue.prototype.$affiliateSet = referral => {
+      let existing = Cookie.get('_ref')
+      if (existing != 'undefined') {
+        Cookie.set('_ref', referral, {
+          expires: 30
+        })
+      }
+    }
+    Vue.prototype.$affiliateGet = () => {
+      let existing = Cookie.get('_ref')
+      if (existing != 'undefined') {
+        return existing
+      } else {
+        return ''
+      }
+    }
   }
 }
 
