cdac1e03eefcbe4661126b0b73c3da92296030a1
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-10-28 18:14:59 +0700

    fixing mobile responsive image side by side

--- components/ImageParagraphSideBySide.vue
+++ components/ImageParagraphSideBySide.vue
@@ -1,21 +1,21 @@
 <template>
   <div
-    class="row img-p-sbs py-5 d-flex align-items-center px-md-4 mb-5 mt-md-5"
+    class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-5"
     v-if="rtl"
   >
-    <div class="col-12 col-md-6 pr-0 pr-md-5 pt-md-0 pt-3">
+    <div class="col-12 col-md-6 pr-0 pr-md-5 pt-md-0 pt-3 order-last order-md-first">
       <h1 class="mb-4" v-if="typeof title == 'object'">
         <div class="title-1" v-for="text in title" :key="text">{{ text }}</div>
       </h1>
       <h1 class="title-1 mb-4" v-else>{{ title }}</h1>
       <p v-for="(text, i) in descriptions" :key="i">{{ text }}</p>
     </div>
-    <div class="col-12 col-md-6 text-right pt-md-0 pt-3" >
+    <div class="col-12 col-md-6 text-right pt-md-0 pt-3 order-first" >
       <video-embed :src="videoUrl" v-if="videoUrl" />
       <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else />
     </div>
   </div>
-  <div class="row img-p-sbs py-5 d-flex align-items-center px-md-4 mb-5 mt-md-5" v-else>
+  <div class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-5" v-else>
     <div class="col-12 col-md-6 pt-md-0 pt-3">
       <video-embed :src="videoUrl" v-if="videoUrl" />
       <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else />
