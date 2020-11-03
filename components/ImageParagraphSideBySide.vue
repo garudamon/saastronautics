0be906b0c9c87e27af63afd1b359046650494e04
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-11-03 15:07:41 +0700

    fixing bug animate on scroll on about mobile responsiveness

--- components/ImageParagraphSideBySide.vue
+++ components/ImageParagraphSideBySide.vue
@@ -10,13 +10,13 @@
       <h1 class="title-1 mb-4" v-else>{{ title }}</h1>
       <p v-for="(text, i) in descriptions" :key="i">{{ text }}</p>
     </div>
-    <div class="col-12 col-md-6 text-right pt-md-0 pt-3 order-first" >
+    <div class="col-12 col-md-6 text-right pt-md-0 pt-3 order-first aos" >
       <video-embed :src="videoUrl" v-if="videoUrl" />
       <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else />
     </div>
   </div>
   <div class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-5" v-else>
-    <div class="col-12 col-md-6 pt-md-0 pt-3">
+    <div class="col-12 col-md-6 pt-md-0 pt-3 aos">
       <video-embed :src="videoUrl" v-if="videoUrl" />
       <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else />
     </div>
@@ -36,4 +36,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.aos {
+  overflow-x: hidden;
+}
+</style>
