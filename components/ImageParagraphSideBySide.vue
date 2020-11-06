3b608cccfd34b5dd1a56c0c982d9fd30a2e953d3
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-11-06 15:51:38 +0700

    fixing pop up imagepsbs

--- components/ImageParagraphSideBySide.vue
+++ components/ImageParagraphSideBySide.vue
@@ -10,21 +10,17 @@
       <h1 class="title-1 mb-4" v-else>{{ title }}</h1>
       <p v-for="(text, i) in descriptions" :key="i">{{ text }}</p>
     </div>
-    <div class="col-12 col-md-6 text-right pt-md-0 pt-3 order-first aos" >
+    <div class="col-12 col-md-6 text-right pt-md-0 pt-3 order-first aos">
       <video-embed :src="videoUrl" v-if="videoUrl" />
       <!-- <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else /> -->
-      <div :data-aos="fadeType" data-aos-duration="1000" v-else>
-        <VuePureLightbox :thumbnail="imageUrl" :images="[imageUrl]" :alternate-text="title"></VuePureLightbox>
-      </div>
+      <VuePureLightbox :thumbnail="imageUrl" :images="[imageUrl]" :alternate-text="title" v-else></VuePureLightbox>
     </div>
   </div>
   <div class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-3" v-else>
     <div class="col-12 col-md-6 pt-md-0 pt-3 aos">
       <video-embed :src="videoUrl" v-if="videoUrl" />
       <!-- <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else /> -->
-      <div :data-aos="fadeType" data-aos-duration="1000" v-else>
-        <VuePureLightbox :thumbnail="imageUrl" :images="[imageUrl]" :alternate-text="title"></VuePureLightbox>
-      </div>
+      <VuePureLightbox :thumbnail="imageUrl" :images="[imageUrl]" :alternate-text="title" v-else></VuePureLightbox>
     </div>
     <div class="col-12 col-md-6 pt-md-0 pt-3">
       <h1 class="mb-4" v-if="typeof title == 'object'">
