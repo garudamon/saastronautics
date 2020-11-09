2c07fe9b909d91bf9e33b4964a89d5a8a4d0feec
Mohamad Ikbal <mohamadikbal.mi@gmail.com>
2020-11-09 12:11:46 +0700

    add padding for lightbox on imagepsbsdeal

--- components/ImageParagraphSideBySideDeals.vue
+++ components/ImageParagraphSideBySideDeals.vue
@@ -3,24 +3,37 @@
     class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-3"
     v-if="rtl"
   >
-    <div class="col-12 col-md-6 pr-0 pr-md-5 pt-md-0 pt-3 order-last order-md-first">
+    <div
+      class="col-12 col-md-6 pr-0 pr-md-5 pt-md-0 pt-3 order-last order-md-first"
+    >
       <h1 class="mb-4" v-if="typeof title == 'object'">
         <div class="title-1" v-for="text in title" :key="text">{{ text }}</div>
       </h1>
       <h1 class="title-1 mb-4" v-else>{{ title }}</h1>
       <p v-for="(text, i) in descriptions" :key="i">{{ text }}</p>
     </div>
-    <div class="col-12 col-md-6 text-right pt-md-0 pt-3 order-first aos">
+    <div class="col-12 col-md-6 text-center text-md-right pt-md-0 pt-3 order-first aos">
       <video-embed :src="videoUrl" v-if="videoUrl" />
-      <!-- <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else /> -->
-      <VuePureLightbox :thumbnail="imageUrl" :images="[imageUrl]" :alternate-text="title" v-else></VuePureLightbox>
+      <VuePureLightbox
+        :thumbnail="imageUrl"
+        :images="[imageUrl]"
+        :alternate-text="title"
+        v-else
+      ></VuePureLightbox>
     </div>
   </div>
-  <div class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-3" v-else>
+  <div
+    class="row img-p-sbs py-md-3 d-flex align-items-center px-md-4 mb-5 mt-md-3"
+    v-else
+  >
     <div class="col-12 col-md-6 pt-md-0 pt-3 aos">
       <video-embed :src="videoUrl" v-if="videoUrl" />
-      <!-- <img :src="imageUrl" :alt="title" :data-aos="fadeType" data-aos-duration="1000" v-else /> -->
-      <VuePureLightbox :thumbnail="imageUrl" :images="[imageUrl]" :alternate-text="title" v-else></VuePureLightbox>
+      <VuePureLightbox
+        :thumbnail="imageUrl"
+        :images="[imageUrl]"
+        :alternate-text="title"
+        v-else
+      ></VuePureLightbox>
     </div>
     <div class="col-12 col-md-6 pt-md-0 pt-3">
       <h1 class="mb-4" v-if="typeof title == 'object'">
@@ -33,19 +46,25 @@
 </template>
 
 <script>
-import VuePureLightbox from 'vue-pure-lightbox';
-import styles from 'vue-pure-lightbox/dist/VuePureLightbox.css';
+import VuePureLightbox from 'vue-pure-lightbox'
+import styles from 'vue-pure-lightbox/dist/VuePureLightbox.css'
 
 export default {
   components: {
     VuePureLightbox
   },
-  props: ['imageUrl', 'title', 'descriptions', 'rtl', 'videoUrl', "fadeType"],
+  props: ['imageUrl', 'title', 'descriptions', 'rtl', 'videoUrl', 'fadeType']
 }
 </script>
 
 <style lang="scss" scoped>
 .aos {
   overflow-x: hidden;
+}
+</style>
+
+<style>
+.lightbox__image img {
+  padding: 3.5vmax
 }
 </style>
